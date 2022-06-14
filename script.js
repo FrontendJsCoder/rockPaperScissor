8b5d2e10d81ff98f89c7b708601ce5887e331c7d
sizwe <nkosiczwe@gmail.com>
2022-06-14 21:05:39 +0200

    add checker method for user and computer gameplay

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ let choices_object = {
                         'paper' : 'draw'
                         }
                     }
+function checker(input){
+    var choices = ['rock', 'paper', 'scissor'];
+    var num = Math.floor(Math.random()*3);
+
+    document.getElementById('comp_choice').innerHTML =
+    ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
+
+    document.getElementById('user_choice').innerHTML =
+    ` You choose <span> ${input.toUpperCase()} </span>`;
+
+    let computer_choice = choices[num];
+}

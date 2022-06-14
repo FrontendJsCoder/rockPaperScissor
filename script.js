e92e3cccd3c5288821ce823d43c67fbf9676f8e5
sizwe <nkosiczwe@gmail.com>
2022-06-14 21:13:37 +0200

    add switch case to control game score

--- script.js
+++ script.js
@@ -32,4 +32,16 @@ function checker(input){
     ` You choose <span> ${input.toUpperCase()} </span>`;
 
     let computer_choice = choices[num];
+
+    switch(choices_object[input][computer_choice]){
+        case 'win':
+            result_ref.style.cssText = "background-color: #cefdce; color: #d32f2f";
+            result_ref.innerHTML = "YOU LOSE";
+            computer_score++;
+            break;
+        default:
+            result_ref.style.cssText = "background-color: #e5e5e5; color: #808080"
+            result_ref.innerHTML = "DRAW";
+            break;
+    }
 }
